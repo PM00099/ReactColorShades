0233d3ab657ebbdf3539891c16af7d907d99b195
Priyank Mithaiwala <priyankmithaiwala@gmail.com>
2020-08-19 15:25:00 +0530

    Updates

---
+++ precache-manifest.ef6787523a190e92a42e0568f5b140ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a7509d330728aa94040abe57a9c0d65",
+    "url": "/ReactColorShades/index.html"
+  },
+  {
+    "revision": "0716dd4ce19fa620d3d8",
+    "url": "/ReactColorShades/static/css/main.434f2f7b.chunk.css"
+  },
+  {
+    "revision": "7987beffbac366cbbe18",
+    "url": "/ReactColorShades/static/js/2.7b80f001.chunk.js"
+  },
+  {
+    "revision": "62afbf7b6a84202ba7762582324f8a1a",
+    "url": "/ReactColorShades/static/js/2.7b80f001.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0716dd4ce19fa620d3d8",
+    "url": "/ReactColorShades/static/js/main.a25b16e4.chunk.js"
+  },
+  {
+    "revision": "4943c513d6141176d6ff",
+    "url": "/ReactColorShades/static/js/runtime-main.f1f91646.js"
+  },
+  {
+    "revision": "b362f89bc335e696ff1f33c64a11d9cc",
+    "url": "/ReactColorShades/static/media/sound1.b362f89b.mp3"
+  }
+]);
